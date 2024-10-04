9858df07112e53e1105795fa75cd957eb461583e
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-10-04 17:22:11 +0200

    sql(insert): exo 1 ~ 3

---
+++ beginner/mysql/insert/exo3.sql
@@ -0,0 +1,15 @@
+-- Seconde façon d'insérer une ligne en base de données (recommandée)
+
+INSERT INTO `users` (
+    `lastname`,
+    `firstname`,
+    `gender`,
+    `date_of_birth`,
+    `city`
+) VALUES (
+    "Smith",
+    "Will",
+    "M",
+    "2007-07-07",
+    "London"
+);
