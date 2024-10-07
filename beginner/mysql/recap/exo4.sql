427fdfe2806318b9c4a1ee7a3ebfc4a4545b2308
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-10-07 18:30:14 +0200

    mysql(recap): exo 1 ~ 5

---
+++ beginner/mysql/recap/exo4.sql
@@ -0,0 +1,8 @@
+-- Sélectionner le nombre de personnes qui sont nées avant 2000, regroupées par
+-- leur genre.
+--
+-- En donnant comme titre à ce nombre : « Le nombre des personnes nées avant 2000 ».
+SELECT gender, COUNT(*) as "Le nombre des personnes nées avant 2000"
+FROM users
+WHERE date_of_birth < "2000-01-01"
+GROUP BY gender;
---
+++ beginner/mysql/recap/exo5.sql
@@ -0,0 +1,4 @@
+-- Sélectionner deux enregistrements à partir du quatrième enregistrement.
+SELECT *
+FROM users
+LIMIT 3,2;
