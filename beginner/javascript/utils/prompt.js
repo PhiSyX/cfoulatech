7b617a400a3ffc1151ae7328ce90c5ab7b9d93f8
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-10-08 22:55:33 +0200

    refactor: prompt & confirm

--- beginner/javascript/utils/prompt.js
+++ beginner/javascript/utils/prompt.js
@@ -1,32 +1,64 @@
 /**
+ * Invite l'utilisateur à entrer un mot, une phrase...
+ * 
  * @param {string} ask - Question à demander l'utilisateur.
- * @param {string | null} def - Valeur par défaut
+ * @param {string|null} [def=null] - valeur par défaut
  * @returns {Promise<string|null>}
  */
-export async function prompt(ask, def = null) {
-	if (!Object.hasOwn(globalThis, "prompt")) {
-		const { createInterface } = await import("node:readline/promises");
-		const { stdin, stdout } = await import("node:process");
+export async function prompt(ask, def) {
+	let required = def === undefined ? true : def !== null;
 
-		let readline = createInterface({
-			input: stdin,
-			output: stdout,
-		});
+	def ??= null;
 
-		let response = await readline.question(ask + " ").catch((_) => def || "");
-		readline.close();
+	if (Object.hasOwn(globalThis, "prompt")) {
+		// NOTE: Browser env
+		let response;
+
+		do {
+			response = globalThis.prompt(ask, def || "");
+
+			if (required && !response) {
+				response = def;
+			}
+		} while (typeof response !== "string");
 
 		return response;
 	}
 
-	return globalThis.prompt(ask, def ?? "");
+	// NOTE: Node.js env
+	const { createInterface } = await import("node:readline/promises");
+	const { stdin, stdout } = await import("node:process");
+
+	let readline = createInterface({
+		input: stdin,
+		output: stdout,
+	});
+
+	let def_s = def && def !== "0" ? `[${def} par défaut] ` : "";
+
+	let response;
+
+	do {
+		response = await readline
+			.question(`${ask} ${def_s}`)
+			.catch((_) => def || "");
+		if (required && !response) {
+			response = def;
+		}
+	} while (typeof response !== "string");
+
+	readline.close();
+
+	return response;
 }
 
 /**
+ * Invite l'utilisateur à entrer un chiffre, nombre...
+ * 
  * @param {string} ask - Question à demander l'utilisateur.
- * @param {number} def - Valeur par défaut
+ * @param {number|null} [def] - valeur par défaut
  * @returns {Promise<number>}
  */
-export async function prompt_number(ask, def = 0) {
-	return prompt(ask, def.toString()).then(Number);
+export async function prompt_number(ask, def = null) {
+	return prompt(ask, def?.toString()).then(Number);
 }
