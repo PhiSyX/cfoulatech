39d8df4dc38743c098bf1af820a56eb5eba8bc10
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-10-15 18:07:42 +0200

    js: exo 11 ~ 15

---
+++ beginner/javascript/functions/exo12.js
@@ -0,0 +1,12 @@
+import { write } from "../utils/write.js";
+
+/**
+ * @param {number} B - base
+ * @param {number} h - hauteur
+ * @returns {number}
+ */
+function calcul_triangle_area(B, h) {
+	return 0.5 * (B * h);
+}
+
+write(calcul_triangle_area(10, 5));
---
+++ beginner/javascript/functions/exo13.js
@@ -0,0 +1,65 @@
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+export function via_for(str) {
+	let new_str = "";
+
+	for (let index = str.length - 1; index >= 0; index--) {
+		let ch = str[index];
+		new_str += ch;
+	}
+
+	return new_str;
+}
+
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+export function recursively(str) {
+	if (str.length === 0) {
+		return "";
+	}
+	return recursively(str.slice(1)) + str.charAt(0);
+}
+
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+export function natively(str) {
+	return Array.from(str).reverse().join("");
+}
+
+let output_1 = via_for("Salut");
+let expected_output_1 = "tulaS";
+
+console.log(
+	output_1,
+	"est équivalent de",
+	expected_output_1,
+	"?",
+	output_1 === expected_output_1,
+);
+
+let output_2 = recursively("Bonjour");
+let expected_output_2 = "ruojnoB";
+
+console.log(
+	output_2,
+	"est équivalent de",
+	expected_output_2,
+	"?",
+	output_2 === expected_output_2,
+);
+
+let output_3 = natively("Bonjour");
+let expected_output_3 = "ruojnoB";
+console.log(
+	output_3,
+	"est équivalent de",
+	expected_output_3,
+	"?",
+	output_3 === expected_output_3,
+);
