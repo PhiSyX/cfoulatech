80d1e0c05a536a7ec6446f664c4dc2caa158419c
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-11-04 16:44:10 +0100

    mysql: exo 15 ~ 17

---
+++ beginner/mysql/select/exo16.sql
@@ -0,0 +1,6 @@
+-- Afficher la racine carrée des poids 2 chiffres après la virgule; le prénom et nom
+SELECT
+    TRUNCATE(SQRT(weight_kg),2),
+    firstname,
+    lastname
+FROM users;
---
+++ beginner/mysql/select/exo17.sql
@@ -0,0 +1,6 @@
+-- Afficher la racine carrée des poids arrondis; le prénom et nom
+SELECT
+    ROUND(SQRT(weight_kg)),
+    firstname,
+    lastname
+FROM users;
