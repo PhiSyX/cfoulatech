780ad9c640be80e3fda13e1f10d48ba2ff8be3e3
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-11-08 00:25:45 +0100

    mysql(date): exo 1 ~ 5

---
+++ beginner/mysql/date/exo3.sql
@@ -0,0 +1,7 @@
+-- Affichez-moi toutes les femmes et autres(X) qui ont moins de 36 ans, ordonné
+-- du plus vieux au plus jeune.
+
+SELECT *, TIMESTAMPDIFF(YEAR, date_of_birth, CURRENT_DATE) AS age FROM users 
+WHERE gender IN('F', 'X') 
+  AND DATE_SUB(CURRENT_DATE, INTERVAL 36 YEAR) < date_of_birth
+ORDER BY date_of_birth ASC;
---
+++ beginner/mysql/date/exo5.sql
@@ -0,0 +1,7 @@
+-- Affichez-moi les utilisateurs qui ont entre 10 et 25 ans, et ordonnez-les du
+-- plus vieux au plus jeune.
+
+SELECT *, TIMESTAMPDIFF(YEAR, date_of_birth, CURRENT_DATE) AS age FROM users 
+WHERE DATE_SUB(CURRENT_DATE, INTERVAL 26 YEAR) < date_of_birth
+  AND DATE_SUB(CURRENT_DATE, INTERVAL 10 YEAR) > date_of_birth
+ORDER BY date_of_birth ASC;
