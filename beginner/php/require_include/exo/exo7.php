b5aa9154bddabbb0be78c86ced06fe823d5e63d0
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-11-17 23:21:33 +0100

    refactor(php): req fns exo7

--- beginner/php/require_include/exo/exo7.php
+++ beginner/php/require_include/exo/exo7.php
@@ -2,27 +2,33 @@
 
 require dirname(__DIR__) . "/fonctions_require.php";
 
+const ADD = 1;
+const SUB = 2;
+const MUL = 3;
+const DIV = 4;
+const DIV_BY_ZERO = 0;
+
 function calc(int $op, float $l, float $r): float
 {
     switch ($op) {
-        case 1: return add($l, $r);
-        case 2: return sub($l, $r);
-        case 3: return mul($l, $r);
-        case 4: return div($l, $r);
+        case ADD: return add($l, $r);
+        case SUB: return sub($l, $r);
+        case MUL: return mul($l, $r);
+        case DIV: return div($l, $r);
     }
 }
 
 if (yes_or_no("Voulez-vous faire un calcul?")) {
     $op = readline(
         "Veuillez introduire une opération entre 1 et 4 (" .
-            "1 = Addition, " .
-            "2 = Soustraction, " .
-            "3 = Multiplication, " .
-            "4 = Division" .
+            ADD . " = Addition, " .
+            SUB . " = Soustraction, " .
+            MUL . " = Multiplication, " .
+            DIV . " = Division" .
         ") : "
     );
 
-    if ( ! ($op >= 1 && $op <= 4)) {
+    if ( ! ($op >= ADD && $op <= DIV)) {
         echo "Vous avez introduit autre chose qu'un chiffre entre 1 et 4\n";
         return;
     }
@@ -31,10 +37,10 @@ if (yes_or_no("Voulez-vous faire un calcul?")) {
     $nb2 = null;
     do {
         $nb2 = readline("Votre second nombre : ");
-        if ($op == 4 && $nb2 == 0) {
-            echo "Vous avez tenté une division par 0, recommencez\n";
+        if ($op == DIV && $nb2 == DIV_BY_ZERO) {
+            echo "Vous avez tenté une division par 0, recommencez...\n";
         }
-    } while ($op == 4 && $nb2 == 0);
+    } while ($op == DIV && $nb2 == DIV_BY_ZERO);
 
     if ( ! (is_numeric($nb1) && is_numeric($nb2))) {
         echo "Veuillez introduire 2 nombres\n";
