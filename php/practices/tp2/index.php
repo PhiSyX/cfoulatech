697e5b6e3ffa28fc9d40bcfda0909e02edd2192c
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-12-03 23:42:29 +0100

    php: ref tp2

--- php/practices/tp2/index.php
+++ php/practices/tp2/index.php
@@ -16,31 +16,158 @@ include "./includes/layouts/header.php";
 ?>
 
 <section id="home-page">
-	<img src="./assets/img/logo.png" alt="Logo">
+	<img src="./assets/img/minecraft.jpg" alt="Minecraft" width="200" height="200">
 
 	<article>
-		<h1>Bank Root</h1>
-
-		<h2>
-			Travailler dans une banque,
-			c'est barbant qu'ils disent.
-			Ca nous fait rire...
-		</h2>
+		<h1>L'histoire de la formation WEB de CFITECH et de <strong>Timothy</strong></h1>
 
 		<p>
-			Les Top Employer 2024 ont été annoncés et nous pouvons vous révéler
-			que <strong>Bank Root</strong> a été reconnu comme
-			Top Employer 2024 de Belgique et ce pour la 10e fois d'affilé.
-			Une réussite dont nous pouvons être fiers.
-			Et tout cela grâce à nos employés qui contribuent chaque jour à
-			faire de <strong>Bank Root</strong> un endroit formidable.
+			<strong>Timothy</strong> a un rêve depuis petit, celui de devenir
+			développeur de jeux vidéos, il rêve de créer ses propres mondes
+			virtuels, des aventures épiques et des personnages mémorables.
 		</p>
 
 		<p>
-			Un lieu de travail agréable, un bon équilibre entre vie professionnelle
-			et vie privée et de nombreuses possibilités de développement vous
-			semblent-ils indispensables dans un emploi ?
-			<strong>Alors vous êtes au bon endroit chez Bank Root !</strong>
+			Un jour, il décida de se lancer dans une formation pour réaliser son
+			rêve : devenir développeur de jeux vidéo.
+
+			<strong>Mais voilà le hic.</strong> Par un curieux malentendu,
+			<strong>Timothy</strong> s'inscrivit dans une formation de
+			développeur <em>WEB</em> chez <strong>CFITECH</strong>.
+		</p>
+
+		<h2>L'HTML/CSS</h2>
+
+		<p>
+			Dès les premiers cours avec <strong>Benoît</strong>, le prof
+			d'HTML/CSS. <strong>Timothy</strong> se sentait déjà un peu décalé.
+			<strong>Benoît</strong>, un chic type mais farceur, lui disait
+			souvent :
+		</p>
+
+		<blockquote>
+			"Bon, <strong>Timothy</strong>, tes balises <code>&lt;div></code>,
+			là... elles ne sont pas prêtes pour le <strong>Game of the Year
+				Awwwards.com</strong>, hein ! 😉"
+		</blockquote>
+
+		<p>
+			Tout le monde a commencé à rire dès lors que
+			<strong>Jeremy</strong>, <strong>Mike</strong> et <strong>Say</strong>
+			aient commencé à rire en premiers, parce qu'ils avaient compris la
+			blague. Certainement pour se rendre socialement acceptables. Sauf
+			<strong>Timothy</strong>, qui n'était pas certain d'avoir envie de
+			coder des sites vitrines ou des portfolios minimalistes.
+		</p>
+
+		<h2>JavaScript</h2>
+
+		<p>
+			Le chaos arriva avec le cours de JavaScript. <strong>Jean-Aimable</strong>
+			un prof très enthousiaste et qui expliquait les choses avec un tic
+			verbal mémorable après chaque explication :
+			<strong>"On est ensemble, les gars ?"</strong>, et souvent à l'aide
+			de métaphores qui, disons-le, ne fonctionnaient pas toujours.
+		</p>
+
+		<blockquote>
+			"Écoutez, les gars, une fonction, c'est comme un sandwich. Les
+			paramètres, c'est ce que vous mettez dedans. Et le retour, c'est...
+			ce que vous mangez ! <br>
+			... <br>
+			.. <br>
+			. <br>
+			On est ensemble, les gars ?"
+		</blockquote>
+
+		<p>
+			Silence dans la classe... Un jour, <strong>Mohammed-Ali</strong> osa
+			répondre : <code>"Pas vraiment, monsieur..."</code>.
+			<strong>Jean-Aimable</strong>, impassible, répondit avec un sourire
+			éclatant : <code>"C'est pas grave, moi je suis avec vous !"</code>
+		</p>
+
+		<p>
+			<strong>Timothy</strong>, pensif au fond de la classe, se demanda
+			vaguement si quelqu'un avait déjà essayé de manger du code.
+		</p>
+
+		<p>
+			Le prof débarquait dans la salle, sourire éclatant, et lançait une
+			petite phrase motivante :
+		</p>
+
+		<blockquote>
+			"Les gars, aujourd'hui, on va rendre vos pages dynamiques avec le
+			DOM ! Vous êtes prêts à faire de la magie ?"
+		</blockquote>
+
+		<p>
+			Mais tout le monde était loin, très loin : perdus dans un code
+			spaghetti de variables, de boucles, et du DOM. Quant à <strong>Timothy</strong>,
+			toujours assis au fond, pensait en lui-même : "De la magie ?
+			J'espère qu'il parle de dragons, The Witcher 3, de Elden Rings..."
+		</p>
+
+		<h2>PHP</h2>
+
+		<p>
+			Pour <strong>Timothy</strong>, PHP était un peu comme être coincé
+			dans un jeu dans une salle sombre avec une lampe torche qui
+			clignote. Il savait qu'il fallait avancer, mais chaque ligne de code
+			le plongeait un peu plus dans le désespoir. Rapidement, les choses
+			se compliquait. Entre les variables précédées d'un dollar
+			(<code>$</code>), les interminables blocs de <code>if</code>, les
+			concaténations étranges avec des points (<code>.</code>), et les
+			erreurs d'XDEBUG.
+		</p>
+
+		<pre>
+( ! ) Parse error: syntax error, unexpected variable "$title" in /tp2/login.php on line 12
+		</pre>
+
+		<p>
+			<strong>Timothy</strong> commença à perdre patience. C'en devenait maladif.
+		</p>
+
+		<blockquote>
+			"Qu'est-ce qui va pas avec ma pu**** de variable $title"
+		</blockquote>
+
+		<p>
+			Le clavier assis sur le bureau, observant
+			<strong>Timothy</strong> taper frénétiquement sur ses touches. Il
+			avait vu beaucoup de choses dans sa vie le clavier, mais jamais il
+			n'avait vu quelqu'un aussi en colère que <strong>Timothy</strong>.
+		</p>
+
+		<p>
+			Le clavier avait l'impression que <strong>Timothy</strong> le
+			traitait avec mépris, frappant ses touches avec une telle force
+			qu'il craignait pour sa vie. Il avait essayé de calmer
+			<strong>Timothy</strong> en émettant des bips gentils chaque fois
+			qu'il appuyait sur une touche, mais rien n'y faisait.
+		</p>
+
+		<p>
+			<strong>Timothy</strong> était entrain d'écrire du code PHP, et il
+			semblait avoir des problèmes avec son programme. Le clavier ne
+			comprenait pas pourquoi <strong>Timothy</strong> était si agacé,
+			après tout, c'était juste un <strong>point-virgule</strong> manquant.
+		</p>
+
+		<p>
+			Mais plus <strong>Timothy</strong> tapait, plus il devenait
+			agressif. Le clavier commençait à être vraiment inquiet, craignant
+			que <strong>Timothy</strong> ne finisse par lui faire du mal.
+		</p>
+
+		<p>
+			Finalement, après des heures de travail acharné,
+			<strong>Timothy</strong> jeta l'éponge. Il ferma <strong>Visual
+			Studio Code</strong> en jurant, et ouvra son jeu favori,
+			<strong>MINECRAFT</strong> pour se défouler sur ma compagne : la
+			souris.
 		</p>
 	</article>
 </section>
