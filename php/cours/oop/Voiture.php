9fc09cc66e53d9cdaf6a827fcdebd32701242efe
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-01-14 18:32:52 +0100

    php: oop

--- php/cours/oop/Voiture.php
+++ php/cours/oop/Voiture.php
@@ -7,35 +7,78 @@ class Voiture
     /**
      * Couleur de la voiture
      */
-    private string $couleur;
+    private string $couleur = "Blanche";
     /**
      * Poids de la voiture
      */
-    private float $poids;
+    private float $poids = 1500.0;
     /**
      * Prix de la voiture
      */
-    private int $prix;
+    private int $prix = 7800;
 
     /**
      * Permet de construire la classe Voiture en un type `objet Voiture`
      */
-    public function __construct(string $couleur, float $poids, int $prix)
+    //public function __construct(string $couleur, float $poids, int $prix)
+    //{
+    //    $this->couleur = $couleur;
+    //    $this->poids = $poids;
+    //    $this->prix = $prix;
+    //}
+
+    /** Les getters / setters */
+
+    public function getCouleur(): string
+    {
+        return $this->couleur;
+    }
+
+    public function setCouleur(string $couleur): void
     {
         $this->couleur = $couleur;
-        $this->poids = $poids;
+    }
+
+    public function getPrix(): int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): void
+    {
         $this->prix = $prix;
     }
+
+    public function getPoids(): float
+    {
+        return $this->poids;
+    }
+
+    public function setPoids(float $poids): void
+    {
+        $this->poids = $poids;
+    }
+
+	public function addPrix(int $prix): void
+	{
+		$this->prix += $prix;
+	}
 
     /** Les comportements */
 
     /**
-     * Démarre la voiture
+     * Cette fonction démarre la voiture
      */
-    public function demarrer(): void {}
+    public function demarrer(): void
+    {
+        echo "La voiture {$this->couleur} a démarré";
+    }
 
     /**
-     * Accélère la voiture
+     * Cette fonction accélère la voiture
      */
-    public function accelerer(): void {}
+    public function accelerer(): void
+    {
+        echo "La voiture {$this->couleur} accélère";
+    }
 }
--- php/cours/oop/main.php
+++ php/cours/oop/main.php
@@ -11,8 +11,71 @@ var_dump($date2);
 
 require "./Voiture.php";
 
+/*
 $maVoiture = new Voiture("blanche", 1500.0, 7800);
-
-// $maVoiture->demarrer();
-
+$maVoiture->demarrer();
+echo "<br>";
+$maVoiture->accelerer();
 var_dump($maVoiture);
+*/
+
+/*
+$maVoiture = new Voiture();
+$maVoiture->couleur = "Blanche";
+$maVoiture->poids = 79.9;
+$maVoiture->prix = 500;
+
+echo "La couleur de la voiture est : <strong>" . $maVoiture->couleur . "</strong> <br>";
+echo "La poids   de la voiture est : <strong>" . $maVoiture->poids   . "</strong>Kg <br>";
+echo "Le prix    de la voiture est : <strong>" . $maVoiture->prix    . "</strong>€ <br>";
+
+$maVoiture->couleur = "Noire";
+echo "Ma nouvelle couleur est : <strong>" . $maVoiture->couleur    . "</strong> <br>";
+*/
+
+$maTwingoRouge = new Voiture();
+
+$maTwingoRouge->setCouleur("Rouge");
+$maTwingoRouge->setPoids(3520.53);
+$maTwingoRouge->setPrix(2500);
+
+echo "La couleur de la voiture est : <strong>" . $maTwingoRouge->getCouleur() . "</strong>   <br>";
+echo "La poids   de la voiture est : <strong>" . $maTwingoRouge->getPoids()   . "</strong>Kg <br>";
+echo "Le prix    de la voiture est : <strong>" . $maTwingoRouge->getPrix()    . "</strong>€  <br> <br>";
+
+$maTwingoRouge->addPrix(100);
+echo "Ajout de <strong>100€</strong> au prix de la voiture: ";
+
+if ($maTwingoRouge->getPrix() === 2600) {
+	echo "le nouveau prix de la voiture est de : <strong>" . $maTwingoRouge->getPrix()    . "</strong>€  <br>";
+}
+
+echo "--------------------------------<br>";
+
+$maPeugeotVerte = new Voiture();
+
+$maPeugeotVerte->setCouleur("Verte");
+$maPeugeotVerte->setPoids(4020.99);
+$maPeugeotVerte->setPrix(2600);
+
+echo "La couleur de la voiture est : <strong>" . $maPeugeotVerte->getCouleur() . "</strong>   <br>";
+echo "La poids   de la voiture est : <strong>" . $maPeugeotVerte->getPoids()   . "</strong>Kg <br>";
+echo "Le prix    de la voiture est : <strong>" . $maPeugeotVerte->getPrix()    . "</strong>€  <br> <br>";
+
+$maPeugeotVerte->addPrix(100);
+echo "Ajout de <strong>100€</strong> au prix de la voiture: ";
+
+if ($maPeugeotVerte->getPrix() === 2700) {
+	echo "le nouveau prix de la voiture est de : <strong>" . $maPeugeotVerte->getPrix()    . "</strong>€  <br>";
+}
+
+$diff = $maTwingoRouge->getPrix() - $maPeugeotVerte->getPrix();
+
+if ($maTwingoRouge->getPrix() > $maPeugeotVerte->getPrix()) {
+	echo "La voiture rouge est plus chère que la voiture verte, il y a une ";
+	echo "différence de : <strong>$diff</strong>€";
+} else {
+	$diff = abs($diff);
+	echo "La voiture verte est plus chère que la voiture rouge, il y a une ";
+	echo "différence de : <strong>$diff</strong>€";
+}
