60e54a62fc8f158c781d3e4406b8ea65632a81ea
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-01-21 20:25:33 +0100

    mysql: show create table

--- mysql/cours/02-table/04-table-copy.sql
+++ mysql/cours/02-table/04-table-copy.sql
@@ -1,2 +1,9 @@
--- Copie une table (users) vers une autre (users2)
+-- Récupérer la requête SQL de la création d'une table avec tous les champs.
+-- Copier la requête et remplacer le nom de la table par la nouvelle, et
+-- executer. La requête peut fournir les auto incrémentations sur les ID si
+-- elles sont présentes. Il faut retirer l'instruction `AUTO_INCREMENT=X`.
+SHOW CREATE TABLE users;
+
+-- Sinon :
+-- Copie une table (users) vers une autre (users2) sans les données.
 CREATE TABLE users2 LIKE users;
