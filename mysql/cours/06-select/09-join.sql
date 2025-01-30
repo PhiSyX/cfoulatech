29964b4851f82f97f8f6b226aa28ac771ec2cf87
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-01-30 17:57:14 +0100

    mysql: left/right join

--- mysql/cours/06-select/09-join.sql
+++ mysql/cours/06-select/09-join.sql
@@ -1,5 +1,18 @@
 -- LEFT  JOIN
+SELECT
+    CONCAT(u.firstname, " ", u.lastname) AS Auteurs,
+    a.article_name
+FROM users u
+LEFT JOIN articles a
+ON u.id_user = a.id_user_article;
 
 -- RIGHT JOIN
+
+SELECT
+	CONCAT(u.firstname, " ", u.lastname) AS Auteurs,
+	a.article_name
+FROM users u
+RIGHT JOIN articles a
+ON u.id_user = a.id_user_article;
 
 -- INNER JOIN
