d035b25b0876d9df27214a0c141841c141e4804c
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2025-03-26 20:50:18 +0100

    symfony: home ctrl

--- php/@symfony/TutoSymfony/src/Controller/HomeController.php
+++ php/@symfony/TutoSymfony/src/Controller/HomeController.php
@@ -8,8 +8,19 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-	#[Route('/', name: 'app_home')]
+	#[Route("/", name: "app_home")]
 	public function index(): Response
+	{
+		return $this->render(
+			"home/index.html.twig",
+			[
+				"controller_name" => HomeController::class,
+			]
+		);
+	}
+
+	#[Route("/hello-world", name: "app_hello_world")]
+	public function hello_world(): Response
 	{
 		return new Response("<h1>Hello World</h1>");
 	}
