d230d92f5071dee13e41e7403a5b02a5cb720d13
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2025-04-20 13:42:36 +0200

    uaa05

--- php/practices/vanilla-mvc/config/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-	"sessionKey" => basename(dirname(__DIR__)) . ".user",
-	"loginPath" => "login.php",
-	"logoutPath" => "logout.php",
-	"redirectAfterLogout" => "login.php",
-	"redirectAfterLogin" => "index.php",
-];
