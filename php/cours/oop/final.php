dcf9bd10f7aec2ee922391b3356e26ec6ca9a864
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-04-22 10:47:07 +0200

    php(oop): fix final

--- php/cours/oop/final.php
+++ php/cours/oop/final.php
@@ -4,13 +4,13 @@
  * Nous ne pouvons pas faire hériter une classe définie en final.
  */
 
-final class WorldFinal {}
+final class ExempleFinalHello {}
 
 //
 // Fatal error: Class Hello cannot extend final class World in final.php on line 13/14
 //
-//class ExampleFinalHello1 extends WorldFinal {}
-//final class ExampleFinalHello2 extends WorldFinal {}
+//class ExampleFinalHello1 extends ExempleFinalHello {}
+//final class ExampleFinalHello2 extends ExempleFinalHello {}
 
-$w = new World();
+$w = new ExempleFinalHello();
 var_dump($w);
