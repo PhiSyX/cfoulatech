dc1abb1a2669016e19c5269bf3bfe1208d158644
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-06-03 17:17:40 +0200

    java: hello world

---
+++ java/cours/myFirstJavaProgram/src/MyFirstClass.java
@@ -0,0 +1,10 @@
+import exo.basics.MySecondClass;
+
+public class MyFirstClass
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Bonjour le monde Java !");
+		System.out.println("Ma première application Java !");
+	}
+}
---
+++ java/cours/myFirstJavaProgram/src/exo/basics/MySecondClass.java
@@ -0,0 +1,19 @@
+package exo.basics;
+
+public class MySecondClass
+{
+	public static void main(String[] args)
+	{
+		// Information sur le JDK utilisé
+		System.out.println("=== Information sur l'environment Java ===");
+		System.out.println("Java version: " + System.getProperty("java.version"));
+		System.out.println("Fournisseur JDK: " + System.getProperty("java.vendor"));
+		System.out.println("Répertoire d'installation Java: " + System.getProperty("java.home"));
+
+		// Information sur le système
+		System.out.println();
+		System.out.println("=== Information sur le système ===");
+		System.out.println("Système d'exploitation : " + System.getProperty("os.name"));
+		System.out.println("Nom d'utilisateur : " + System.getProperty("user.name"));
+	}
+}
