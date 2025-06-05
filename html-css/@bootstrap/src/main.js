766a44448f5257bda133ce5ab3ba03b231e80bc1
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-06-05 18:00:03 +0200

    html-css: boostrap form

--- html-css/@bootstrap/src/main.js
+++ html-css/@bootstrap/src/main.js
@@ -1,2 +1,27 @@
 import "./styles.scss";
 import * as bootstrap from "bootstrap";
+
+(() => {
+	/**
+	 * @type {NodeListOf<HTMLFormElement>}
+	 */
+	const formsList = document.querySelectorAll(".needs-validation");
+	const forms = Array.from(formsList);
+	for (let form of forms) {
+		form.addEventListener(
+			"submit",
+			(event) => {
+				if (!form.checkValidity()) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+				form.classList.add("was-validated");
+			},
+			false
+		);
+
+		form.addEventListener("reset", () => {
+			form.classList.remove("was-validated");
+		});
+	}
+})();
