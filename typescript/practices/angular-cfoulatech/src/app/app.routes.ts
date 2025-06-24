eb809687ca02684fd018f1d7b916c25ee3d7cfae
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-06-24 13:08:44 +0200

    typescript(angular): simple project cfoulatech

---
+++ typescript/practices/angular-cfoulatech/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+export const routes: Routes = [
+	{ path: "", redirectTo: "home", pathMatch: "full" },
+	{ path: "home", component: HomeComponent },
+	{ path: "services", component: ServicesComponent },
+	{ path: "contact", component: ContactComponent },
+	{ path: "**", component: NotFoundComponent },
+];
