41570038ac31bbcea029124fd058ed40de2f69ca
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-06-30 15:31:13 +0200

    java: Exemple d'ajout de classes Animal et Dog avec héritage
    Introduit une classe de base Animal et une sous-classe Dog, illustrant l'héritage, la substitution de méthode et un test simple dans DogTest. La classe Dog ajoute une propriété de race et une méthode d'aboiement, et remplace la méthode eat.

---
+++ java/courses/src/exo/basics/klass/inherits/Animal.java
@@ -0,0 +1,23 @@
+package exo.basics.klass.inherits;
+
+public class Animal
+{
+	protected String name;
+	protected int age;
+
+	public Animal(String name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+
+	public void eat()
+	{
+		System.out.printf("%s mange.%n", name);
+	}
+
+	public void sleep()
+	{
+		System.out.printf("%s dort.%n", name);
+	}
+}
---
+++ java/courses/src/exo/basics/klass/inherits/Dog.java
@@ -0,0 +1,34 @@
+package exo.basics.klass.inherits;
+
+public class Dog extends Animal
+{
+	private String breed;
+
+	public Dog(String name, int age, String breed)
+	{
+		super(name, age);
+		this.breed = breed;
+	}
+
+	public void bark()
+	{
+		System.out.printf("%s aboie : Woof%n", name);
+	}
+
+	@Override
+	public void eat()
+	{
+		System.out.printf("%s mange des croquettes.%n", name);
+	}
+}
+
+class DogTest
+{
+	public static void main(String[] args)
+	{
+		Dog laika = new Dog("Laika", 10, "Husky");
+		laika.bark();
+		laika.eat();
+		laika.sleep();
+	}
+}
