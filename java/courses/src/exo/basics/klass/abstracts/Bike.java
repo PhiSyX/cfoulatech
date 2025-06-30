f5601524b5ef378668681f57886a770ac43badbd
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-06-30 15:32:14 +0200

    Ajoute une classe de véhicule abstraite avec des sous-classes de voiture et de vélo
    Introduit une classe Véhicule abstraite avec les propriétés brand et speedMax, ainsi qu'une méthode abstraite start(). Ajoute les sous-classes Car et Bike implémentant start() et inclut une classe VehicleTest pour démontrer l'utilisation et tester les fonctionnalités.

---
+++ java/courses/src/exo/basics/klass/abstracts/Bike.java
@@ -0,0 +1,15 @@
+package exo.basics.klass.abstracts;
+
+public class Bike extends Vehicle
+{
+	public Bike(String brand, int speedMax)
+	{
+		super(brand, speedMax);
+	}
+
+	@Override
+	public void start()
+	{
+		System.out.printf("Le %s démarre à la force des jambes%n", getBrand());
+	}
+}
---
+++ java/courses/src/exo/basics/klass/abstracts/Car.java
@@ -0,0 +1,15 @@
+package exo.basics.klass.abstracts;
+
+public class Car extends Vehicle
+{
+	public Car(String brand, int speedMax)
+	{
+		super(brand, speedMax);
+	}
+
+	@Override
+	public void start()
+	{
+		System.out.printf("La %s démarre avec un moteur", getBrand());
+	}
+}
