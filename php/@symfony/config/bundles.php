da3df918c847227cd24a4792cf32c49921cbe55b
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-09-01 19:39:27 +0200

    php(symfony): JWT

--- php/@symfony/config/bundles.php
+++ php/@symfony/config/bundles.php
@@ -21,4 +21,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
