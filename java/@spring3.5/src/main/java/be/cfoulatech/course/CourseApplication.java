c2e1ae1afafc844be052f47a03703dc04e28e846
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-09-09 16:35:01 +0200

    java(spring): intro

---
+++ java/@spring3.5/src/main/java/be/cfoulatech/course/CourseApplication.java
@@ -0,0 +1,31 @@
+package be.cfoulatech.course;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CourseApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner showBeans(ApplicationContext context)
+	{
+		return args -> {
+			System.out.println("=== BEANS CRÉÉS AUTOMATIQUEMENT ===");
+			String[] beans = context.getBeanDefinitionNames();
+			System.out.println("Nombre total de beans : " + beans.length);
+			System.out.println("Premiers beans : ");
+			for (int i = 0; i < Math.min(10, beans.length); i++) {
+				System.out.println("\t " + (i + 1) + ") " + beans[i]);
+			}
+		};
+	}
+}
+
