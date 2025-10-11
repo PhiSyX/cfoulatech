a8adeb631aeacc89b64e5849cacee000c0098a4b
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-10-11 11:43:34 +0200

    java(spring): thymeleaf avec bdd

--- java/@spring3.5/src/main/java/be/cfoulatech/course/presentation/controller/MemberController.java
+++ java/@spring3.5/src/main/java/be/cfoulatech/course/presentation/controller/MemberController.java
@@ -1,7 +1,9 @@
 package be.cfoulatech.course.presentation.controller;
 
+import be.cfoulatech.course.business.service.MemberService;
 import be.cfoulatech.course.domain.entity.Member;
 import be.cfoulatech.course.domain.enums.MemberStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +14,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 class MemberController
 {
-	private List<Member> dataMembers()
+	private final MemberService memberService;
+
+	private List<Member> exampleDataMembers()
 	{
 		var members = new ArrayList<Member>();
 
@@ -28,7 +33,29 @@ class MemberController
 		return members;
 	}
 
-	@GetMapping(value = "/ex/members", name = "member_index")
+	private List<Member> dataMembers()
+	{
+		return memberService.findAll();
+	}
+
+	@GetMapping(value = "/ex/members", name = "member_ex_index")
+	public String ex_index(Model model)
+	{
+		var members = exampleDataMembers();
+		model.addAttribute("members", members);
+		return "member/index";
+	}
+
+	@GetMapping(value = "/ex/members/{email}", name = "member_ex_details")
+	public String ex_show(@PathVariable String email, Model model)
+	{
+		var members = exampleDataMembers();
+		var member = members.stream().filter(m -> m.getEmail().equals(email)).findFirst().get();
+		model.addAttribute("member", member);
+		return "member/details";
+	}
+
+	@GetMapping(value = "/members", name = "member_index")
 	public String index(Model model)
 	{
 		var members = dataMembers();
@@ -36,12 +63,11 @@ class MemberController
 		return "member/index";
 	}
 
-	@GetMapping(value = "/ex/members/{email}", name = "member_detail")
+	@GetMapping(value = "/members/{email}", name = "member_details")
 	public String show(@PathVariable String email, Model model)
 	{
 		var members = dataMembers();
 		var member = members.stream().filter(m -> m.getEmail().equals(email)).findFirst().get();
-		System.out.println(member);
 		model.addAttribute("member", member);
 		return "member/details";
 	}
